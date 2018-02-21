82ca4ccda7e401b9d9329ce64ca23ccc3152e998
Trainee <Trainee@KGFSL.COM>
2018-02-21 16:56:33 +0530

    first commit

---
+++ src/main/java/com/kgfsl/model/User.java
@@ -0,0 +1,21 @@
+package com.kgfsl.model;
+
+public class User {
+	private String userName;
+	private String gender;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+}
